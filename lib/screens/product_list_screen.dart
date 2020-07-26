aca87638f4f7722fd8543a4265a119f54f3c2871
Hamid Musayev <hemidvsmusayev@gmail.com>
2020-07-26 18:57:57 +0400

    product list completed

--- lib/screens/product_list_screen.dart
+++ lib/screens/product_list_screen.dart
@@ -1,3 +1,6 @@
+import 'package:bloc_app/blocs/cart_bloc.dart';
+import 'package:bloc_app/blocs/product_bloc.dart';
+import 'package:bloc_app/models/cart.dart';
 import 'package:flutter/material.dart';
 
 class ProductListScreen extends StatelessWidget {
@@ -9,10 +12,43 @@ class ProductListScreen extends StatelessWidget {
         actions: <Widget>[
           IconButton(
             icon: Icon(Icons.shopping_cart),
-            onPressed: ()=> Navigator.pushNamed(context, "/cart"),
+            onPressed: () => Navigator.pushNamed(context, "/cart"),
           )
         ],
       ),
+      body: buildProductList(),
     );
+  }
+
+  buildProductList() {
+    return StreamBuilder(
+      initialData: productBloc.getAll(),
+      stream: productBloc.getStream,
+      builder: (context, snapshot) {
+        return snapshot.data.length > 0
+            ? buildProductListItems(snapshot)
+            : Center(
+                child: Text("Data yoxdur"),
+              );
+      },
+    );
+  }
+
+  buildProductListItems(AsyncSnapshot<dynamic> snapshot) {
+    return ListView.builder(
+        itemCount: snapshot.data.length,
+        itemBuilder: (BuildContext context, index) {
+          final list = snapshot.data;
+          return ListTile(
+            title: Text(list[index].name),
+            subtitle: Text(list[index].price.toString()),
+            trailing: IconButton(
+              icon: Icon(Icons.add_shopping_cart),
+              onPressed: () {
+                cartBloc.addToCart(Cart(list[index], 1));
+              },
+            ),
+          );
+        });
   }
 }
